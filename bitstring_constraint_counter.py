9d412a49a44f95b68e023d7d00f92be1c976e824
giragon6 <emmemcdonald66@gmail.com>
2026-01-08 15:22:17 -0500

    e

--- bitstring_constraint_counter.py
+++ bitstring_constraint_counter.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 def next(bitstrings, max_constraint=0, constraints=[]):
     new_bitstrings = []
     for b in bitstrings:
@@ -22,6 +24,25 @@ def get_x(n, constraints=[]):
         bs = next(bs, max_constraint=MAX_LENGTH_CONSTRAINT, constraints=constraints)
     return bs
     
-for i in range(10):
-    print(f"x_{i}={len(get_x(i, constraints=["000", "11"]))}")
+# print("x_0=0")
+# for i in range(20):
+#     print(f"x_{i+1}={len(get_x(i+1, constraints=["0101"]))}")
     
+    
+reps = 20
+combos = ["111","000","100","011","001","110"]
+combos_bs = {}
+
+combos_bs["x"] = []
+for j in range(reps):
+    combos_bs["x"].append(range(j+1))
+
+for c in combos:
+    combos_bs[c] = []
+    for j in range(reps):
+        combos_bs[c].append(get_x(j+1, constraints=c.split(',')))
+
+table_bs = []
+for i in range(len(combos_bs[combos[0]])):
+    table_bs.append([len(combos_bs[c][i]) for c in combos_bs])
+print(tabulate(table_bs, headers=combos))
